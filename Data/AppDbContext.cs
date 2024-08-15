8ab80e52840542eb7cc204420c3861da4115e01d
anonymous <jbrynnbacuta@gmail.com>
2024-08-15 20:29:29 +0800

    add mysql entity framework

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using backend.Models;
+using Microsoft.EntityFrameworkCore;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Product> Products { get; set; }
+    public DbSet<User> Users { get; set; }
+
+    // Configure your model here (optional)
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        // Customize your model here
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,8 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+using backend.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
+
+// Register the DbContext with the dependency injection container
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseMySql(builder.Configuration.GetConnectionString("DefaultConnection"),
+    new MySqlServerVersion(new Version(8, 0, 21)))); // Adjust the MySQL version as per your setup
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
