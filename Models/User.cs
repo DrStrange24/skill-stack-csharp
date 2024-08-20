a8b116e4d2aa8b21704eb634019b12e550b38964
anonymous <jbrynnbacuta@gmail.com>
2024-08-20 23:01:34 +0800

    remove email on user as it is default

--- Models/User.cs
+++ Models/User.cs
@@ -7,7 +7,6 @@ namespace backend.Models
     public class User: IdentityUser
     {
         public int Id { get; set; }
-        public string Email { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
     }
