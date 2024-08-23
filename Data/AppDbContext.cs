e74c3a1790a8f78d8a5a3f158471ef096f7f25e7
anonymous <jbrynnbacuta@gmail.com>
2024-08-23 21:52:44 +0800

    update db default value of some column in a table

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -1,7 +1,6 @@
 ﻿using PersonalWebApp.Models;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
-using System.Reflection.Emit;
 
 public class AppDbContext : IdentityDbContext<User>
 {
@@ -17,32 +16,5 @@ public class AppDbContext : IdentityDbContext<User>
     protected override void OnModelCreating(ModelBuilder builder)
     {
         base.OnModelCreating(builder);  // Ensure the Identity configurations are applied
-
-        MessageConfig(builder);
-    }
-
-    private void MessageConfig(ModelBuilder builder)
-    {
-        // Configure MessageId to have a default GUID value
-        //builder.Entity<Message>()
-        //    .Property(m => m.MessageId)
-        //    .HasDefaultValueSql("UUID()");
-
-        //// Alternatively, you can enforce GUID generation in the application code by setting the default value in the class constructor
-        //builder.Entity<Message>()
-        //    .Property(m => m.SentAt)
-        //    .HasDefaultValueSql("CURRENT_TIMESTAMP");  // Default for SentAt
-
-        //builder.Entity<Message>()
-        //    .HasOne(m => m.Sender)
-        //    .WithMany() // No navigation property in IdentityUser for sent messages
-        //    .HasForeignKey(m => m.SenderId)
-        //    .OnDelete(DeleteBehavior.Cascade);
-
-        //builder.Entity<Message>()
-        //    .HasOne(m => m.Receiver)
-        //    .WithMany() // No navigation property in IdentityUser for received messages
-        //    .HasForeignKey(m => m.ReceiverId)
-        //    .OnDelete(DeleteBehavior.Cascade);
     }
 }
---
+++ Migrations/20240823134310_UpdateMessageDefaultDate.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PersonalWebApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateMessageDefaultDate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "SentAt",
+                table: "Messages",
+                type: "datetime",
+                nullable: false,
+                defaultValueSql: "CURRENT_TIMESTAMP"
+            );
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
