d37d029b175868f3b42aec2a303dc8e4136f9db2
anonymous <jbrynnbacuta@gmail.com>
2024-08-24 10:03:05 +0800

    remove email password

--- Services/Implementations/EmailSender .cs
+++ Services/Implementations/EmailSender .cs
@@ -15,7 +15,7 @@ namespace PersonalWebApp.Services.Implementations
             _smtpClient = new SmtpClient("smtp.gmail.com")
             {
                 Port = 587,
-                Credentials = new NetworkCredential(_fromEmail, "lxwu mtre funu cadd"),
+                Credentials = new NetworkCredential(_fromEmail, ""),
                 EnableSsl = true,
             };
         }
