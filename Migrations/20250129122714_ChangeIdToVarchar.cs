9f9394247de900ddfca417bc738b4b78c1e7139a
anonymous <jbrynnbacuta@gmail.com>
2025-01-29 20:47:44 +0800

    fix bug on user id

---
+++ Migrations/20250129122714_ChangeIdToVarchar.cs
@@ -0,0 +1,130 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PersonalWebApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeIdToVarchar : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            void UpdateUserIdDataType(MigrationBuilder migrationBuilder)
+            {
+                migrationBuilder.AlterColumn<string>(
+                    name: "Id",
+                    table: "aspnetusers",
+                    type: "varchar(255)",
+                    nullable: false,
+                    oldClrType: typeof(Guid),
+                    oldType: "char(36)"
+                );
+            }
+
+            HandleUserForeignKeyMigration(migrationBuilder,UpdateUserIdDataType);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<Guid>(
+                name: "Id",
+                table: "aspnetusers",
+                type: "char(36)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(255)"
+            );
+        }
+
+        private void HandleUserForeignKeyMigration(MigrationBuilder migrationBuilder, Action<MigrationBuilder> updates)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_AspNetUserTokens_AspNetUsers_UserId",
+                table: "aspnetusertokens"
+            );
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_AspNetUserClaims_AspNetUsers_UserId",
+                table: "AspNetUserClaims"
+            );
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_AspNetUserLogins_AspNetUsers_UserId",
+                table: "AspNetUserLogins"
+            );
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_AspNetUserRoles_AspNetUsers_UserId",
+                table: "AspNetUserRoles"
+            );
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Messages_AspNetUsers_ReceiverId",
+                table: "Messages"
+            );
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Messages_AspNetUsers_SenderId",
+                table: "Messages"
+            );
+
+            updates(migrationBuilder);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AspNetUserTokens_AspNetUsers_UserId",
+                table: "aspnetusertokens",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade
+            );
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AspNetUserClaims_AspNetUsers_UserId",
+                table: "AspNetUserClaims",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade
+            );
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AspNetUserLogins_AspNetUsers_UserId",
+                table: "AspNetUserLogins",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade
+            );
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AspNetUserRoles_AspNetUsers_UserId",
+                table: "AspNetUserRoles",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade
+            );
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Messages_AspNetUsers_ReceiverId",
+                table: "Messages",
+                column: "ReceiverId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade
+            );
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Messages_AspNetUsers_SenderId",
+                table: "Messages",
+                column: "SenderId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade
+            );
+        }
+    }
+}
