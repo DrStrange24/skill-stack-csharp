78474e09a5b10f7b3d629d0cc439a53a80b2f643
anonymous <jbrynnbacuta@gmail.com>
2025-01-29 21:12:25 +0800

    UpdateProductId datatype

---
+++ Migrations/20250129130517_ChangeProductIdToVarchar.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PersonalWebApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeProductIdToVarchar : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Id",
+                table: "products",
+                type: "varchar(255)",
+                nullable: false,
+                collation: "ascii_general_ci",
+                oldClrType: typeof(Guid),
+                oldType: "int"
+            );
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
