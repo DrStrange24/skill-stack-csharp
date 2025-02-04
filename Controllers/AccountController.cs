200cf9ce5b25a50fc64ebc90aebf6ce8e0821f3a
anonymous <jbrynnbacuta@gmail.com>
2025-02-04 12:14:38 +0800

    refactor login and signup dto namespace

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -7,7 +7,6 @@ using Microsoft.AspNetCore.Identity.UI.Services;
 using SkillStackCSharp.Services.Interfaces;
 using Microsoft.AspNetCore.Authorization;
 using System.Security.Claims;
-using DTOs.AccountDTOs;
 using SkillStackCSharp.DTOs.AccountDTOs;
 
 namespace SkillStackCSharp.Controllers
--- DTOs/AccountDTOs/LoginDTO.cs
+++ DTOs/AccountDTOs/LoginDTO.cs
@@ -1,6 +1,6 @@
 ﻿using System.ComponentModel.DataAnnotations;
 
-namespace DTOs.AccountDTOs
+namespace SkillStackCSharp.DTOs.AccountDTOs
 {
     public class LoginDTO
     {
