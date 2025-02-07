b54b1f5bf12c8ebdab6517db811bed09207e4042
anonymous <jbrynnbacuta@gmail.com>
2025-02-07 18:04:20 +0800

    reset migrations

--- Migrations/AppDbContextModelSnapshot.cs
+++ Migrations/AppDbContextModelSnapshot.cs
@@ -6,7 +6,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
 
-namespace SkillStackCSharp.Migrations
+namespace skillstackcsharp.Migrations
 {
     [DbContext(typeof(AppDbContext))]
     partial class AppDbContextModelSnapshot : ModelSnapshot
